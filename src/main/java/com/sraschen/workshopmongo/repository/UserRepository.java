e8f465ab6373c8e13594ca017bff3ba22b4210b0
Samuel Raschen <sraschen@yahoo.com.br>
2023-01-31 10:36:20 -0300

    Obtendo um usuário por id

--- src/main/java/com/sraschen/workshopmongo/repository/UserRepository.java
+++ src/main/java/com/sraschen/workshopmongo/repository/UserRepository.java
@@ -7,6 +7,5 @@ import com.sraschen.workshopmongo.domain.User;
 
 @Repository
 public interface UserRepository extends MongoRepository<User, String>{
-
 		
 }
---
+++ src/main/java/com/sraschen/workshopmongo/resources/exception/ResourceExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.sraschen.workshopmongo.resources.exception;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.sraschen.workshopmongo.services.exception.ObjectNotFoundException;
+
+
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e , HttpServletRequest request){
+		String error = "Resource not found.";
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);	
+	}
+	
+	/*
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> Database(DatabaseException e , HttpServletRequest request){
+		String error = "Database error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);	
+	}*/
+}
